c30c108a33b691e30c909ff67f7adbe1f5c8da34
Gajavelli Venkatesh <gajavellisking105@gmail.com>
2019-02-19 14:39:20 +0530

    Add files via upload

---
+++ Language Basics/src/Assignment1.java
@@ -0,0 +1,10 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0] + " Technologies " + args[1]);
+
+	}
+
+}
---
+++ Language Basics/src/Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int v = Integer.parseInt(args[0]);
+		int e = Integer.parseInt(args[1]);
+		int sum = v+e;
+		System.out.println("The sum of " + v + " and " + e + " is " + sum);
+
+	}
+
+}
